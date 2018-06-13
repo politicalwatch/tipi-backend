91d59abaac9a52f41c1af7dab2028932ffdef082
pr3ssh <x@pr3ssh.net>
2018-06-13 18:44:06 +0200

    Add queryset to Initiative model

--- tipi_backend/database/models/initiative.py
+++ tipi_backend/database/models/initiative.py
@@ -1,3 +1,5 @@
+from mongoengine.queryset import queryset_manager
+
 from tipi_backend.database import db
 
 
@@ -29,3 +31,6 @@ class Initiative(db.DynamicDocument):
     meta = {'collection': 'initiatives'}
     # TODO Add indexes https://mongoengine-odm.readthedocs.io/guide/defining-documents.html#indexes
 
+    @queryset_manager
+    def objects(doc_cls, queryset):
+        return queryset.filter(topics__not__size=0).order_by('-updated')
