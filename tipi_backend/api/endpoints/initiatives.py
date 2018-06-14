285fe2c94ae53b31f08c918823cb036809aaa48b
pr3ssh <x@pr3ssh.net>
2018-06-14 14:00:21 +0200

    Clean initiatives endpoint

--- tipi_backend/api/endpoints/initiatives.py
+++ tipi_backend/api/endpoints/initiatives.py
@@ -3,9 +3,11 @@ import logging
 from flask import request
 from flask_restplus import Resource
 from mongoengine.queryset import DoesNotExist
+
 from tipi_backend.api.restplus import api
 from tipi_backend.api.business import search_initiatives, get_initiative
 from tipi_backend.api.validators import validate_id_as_hash
+
 
 log = logging.getLogger(__name__)
 
@@ -29,8 +31,7 @@ ns = api.namespace('initiatives', description='Operations related to initiatives
 @ns.param(name='place', description='Place', type=str, default='', location=['query'], help='Invalid place')
 @ns.param(name='reference', description='Reference', type=str, default='', location=['query'], help='Invalid reference')
 @ns.param(name='type', description='Type', type=str, default='', location=['query'], help='Invalid type')
-# TODO: Terminar de completar los estados
-@ns.param(name='state', description='State', type=str, default='', choices=('Aprobada', 'Rechazada'), location=['query'], help='Invalid state')
+@ns.param(name='state', description='State', type=str, default='', location=['query'], help='Invalid state')
 @ns.param(name='title', description='Title', type=str, default='', location=['query'], help='Invalid title')
 # Common parameters
 @ns.param('limit', 'Limit', type=int, default=20, location=['query'], help='Invalid limit')
