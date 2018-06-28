6596590e25d4b37aed29ba175c714b45a278fc20
pr3ssh <x@pr3ssh.net>
2018-06-28 14:30:42 +0200

    Add related initiatives

--- tipi_backend/database/schemas/initiative.py
+++ tipi_backend/database/schemas/initiative.py
@@ -37,11 +37,28 @@ class InitiativeSchema(ma.ModelSchema):
 class InitiativeExtendedSchema(ma.ModelSchema):
     class Meta:
         model = Initiative
+        model_skip_values = [None]
         model_fields_kwargs = {
                 'author_deputies': {'load_only': True},
                 'author_parliamentarygroups': {'load_only': True},
                 'author_others': {'load_only': True},
+                'tagged': {'load_only': True},
                 }
 
     authors = AuthorsField(attribute='author_parliamentarygroups')
     deputies = DeputiesField(attribute='author_deputies')
+    related = ma.fields.Method(serialize="_related_serializer")
+
+    def _related_serializer(self, obj):
+        related = InitiativeSchema(many=True).dump(Initiative.all(reference=obj['reference']))
+        if related.errors:
+            return []
+        related = [r for r in related.data if r['id'] != obj['id']]
+        self._process_soft_related(related)
+        return related
+
+    # Soft related means that it is related but des not have any topics
+    def _process_soft_related(self, related):
+        for r in related:
+            if len(r['topics']) is 0:
+                del r['id']
