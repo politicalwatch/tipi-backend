37c7749dcad5e84ccea5bfc97f71be37d16384e3
Víctor Ramírez de la Corte <virako.9@gmail.com>
2019-12-23 10:27:31 +0100

    Fix environment vars

--- tipi_backend/settings.py
+++ tipi_backend/settings.py
@@ -10,14 +10,14 @@ class Config:
 
     # Flask-Restplus settings
     SWAGGER_UI_DOC_EXPANSION = env.get('SWAGGER_UI_DOC_EXPANSION', 'list')
-    RESTPLUS_VALIDATE = env.get('RESTPLUS_VALIDATE', True)
-    RESTPLUS_MASK_SWAGGER = env.get('RESTPLUS_MASK_SWAGGER', False)
-    ERROR_404_HELP = env.get('ERROR_404_HELP', False)
+    RESTPLUS_VALIDATE = env.get('RESTPLUS_VALIDATE', "True") == "True"
+    RESTPLUS_MASK_SWAGGER = env.get('RESTPLUS_MASK_SWAGGER', "False") == "True"
+    ERROR_404_HELP = env.get('ERROR_404_HELP', "False") == "True"
 
     # Mongo settings
     MONGODB_SETTINGS = {
         'host': env.get('MONGO_HOST', '0.0.0.0'),
-        'port': env.get('MONGO_PORT', 32817),
+        'port': int(env.get('MONGO_PORT', '27017')),
         'db': env.get('MONGO_DBNAME', 'tipidb'),
         'username': env.get('MONGO_USERNAME', 'tipi'),
         'password': env.get('MONGO_PASSWORD', 'tipi')
