72fb2a9c380a8352c2b9011ddc3bb8abe3d4fc0c
pr3ssh <x@pr3ssh.net>
2020-01-07 17:23:54 +0100

    Separate labels/extract in two methods, one for text and another for files (not implemented yet)

--- tipi_backend/api/endpoints/labels.py
+++ tipi_backend/api/endpoints/labels.py
@@ -1,10 +1,10 @@
 import logging
 
 from flask import request
-from flask_restplus import Namespace, Resource
+from flask_restplus import Namespace, Resource, abort
 from werkzeug.contrib.cache import SimpleCache
 
-from tipi_backend.api.parsers import parser_labels_from_text
+from tipi_backend.api.parsers import parser_labels_from_text, parser_labels_from_file
 from tipi_backend.api.business import extract_labels_from_text, get_tags
 
 
@@ -30,5 +30,26 @@ class LabelsExtractorFromText(LabelsExtractor):
         self.load_tags()
         return extract_labels_from_text(
             request.form['text'],
+            self.tags
+        )
+
+
+@ns.route('/extract-from-file')
+@ns.expect(parser_labels_from_file)
+class LabelsExtractorFromFile(LabelsExtractor):
+    def post(self):
+        """Returns a dictionary of topics and tags matching the file content."""
+        text = ''
+        self.load_tags()
+        file_to_process = request.files['file']
+        '''
+        INSTALL TEXTRACT
+        '''
+        if file_to_process.content_type == 'text/plain':
+            text = ''.join(str(file_to_process.stream.readlines()))
+        else:
+            abort(400, message='Not a valid filetype')
+        return extract_labels_from_text(
+            text,
             self.tags
         )
--- tipi_backend/api/parsers.py
+++ tipi_backend/api/parsers.py
@@ -36,7 +36,10 @@ parser_authors.add_argument('name', type=str, location='args', help='Send a name
 
 
 parser_labels_from_text = reqparse.RequestParser()
-parser_labels_from_text.add_argument(name='text', type=str, location='form', help='Text to be parsed for tags')
+parser_labels_from_text.add_argument(name='text', type=str, required=True, location='form', help='Text to be parsed for tags')
+
+parser_labels_from_file = reqparse.RequestParser()
+parser_labels_from_file.add_argument(name='file', required=True, location='files', help='File to be parsed for tags')
 
 
 class SearchInitiativeParser:
