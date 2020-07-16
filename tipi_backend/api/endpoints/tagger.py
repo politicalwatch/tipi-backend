f732ff03a32d56ab36015a1875d9259ec55e77a8
pr3ssh <x@pr3ssh.net>
2020-07-16 17:47:02 +0200

    More realistic tagger estimated time (1/2x)

--- tipi_backend/api/endpoints/tagger.py
+++ tipi_backend/api/endpoints/tagger.py
@@ -54,7 +54,7 @@ class TaggerExtractor(Resource):
 
             if text_length >= Config.TAGGER_MAX_WORDS:
                 task = tipi_tasks.tagger.extract_tags_from_text.apply_async((text, tags))
-                eta_time = int((text_length / 1000) * 8)
+                eta_time = int((text_length / 1000) * 4)
                 task_id = task.id
                 result = {
                         'status': 'PROCESSING',
