d5d86e86d7e9a47fe373de7684ff7d5902c30a9c
David Hernandez <davidhernandezruiz@gmail.com>
2021-03-11 13:32:28 +0100

    Adds the option to the initiative endpoints to change the serializer used to return the results.

--- tipi_backend/api/parsers.py
+++ tipi_backend/api/parsers.py
@@ -3,28 +3,32 @@ import datetime
 from flask_restplus import reqparse
 from tipi_data.models.parliamentarygroup import ParliamentaryGroup
 from tipi_data.models.initiative_type import InitiativeType
+from tipi_data.schemas.initiative import InitiativeSchema, InitiativeExtendedSchema
 
 from tipi_backend.api.validators import validate_date
 
 
-parser_initiative = reqparse.RequestParser()
+parser_initiatives = reqparse.RequestParser()
 # Common parameters
-parser_initiative.add_argument('page', type=int, default=1, location='args', help='Page number')
-parser_initiative.add_argument('per_page', type=int, default=20, location='args', help='Initiatives per page')
+parser_initiatives.add_argument('page', type=int, default=1, location='args', help='Page number')
+parser_initiatives.add_argument('per_page', type=int, default=20, location='args', help='Initiatives per page')
 # Initiative parameters
-parser_initiative.add_argument('title', type=str, location='args')
-parser_initiative.add_argument('status', type=str, location='args', help='To get the values, check out /initiative-status')
-parser_initiative.add_argument('type', type=str, location='args', help='To get the values, check out /initiative-type')
-parser_initiative.add_argument('reference', type=str, location='args')
-parser_initiative.add_argument('place', type=str, location='args')
-parser_initiative.add_argument('enddate', type=str, location='args', help='Date format must be yyyy-mm-dd')
-parser_initiative.add_argument('startdate', type=str, location='args', help='Date format must be yyyy-mm-dd')
-parser_initiative.add_argument('deputy', type=str, location='args', help='To get the values, check out /deputies')
-parser_initiative.add_argument('author', type=str, location='args', help='To get the values, check out /parliamentary-groups')
-parser_initiative.add_argument('tags', type=str, action='append', location='args', help='To get the values, check out /topics/id')
-parser_initiative.add_argument('subtopics', type=str, action='append', location='args', help='To get the values, check out /topics/id')
-parser_initiative.add_argument('topic', type=str, location='args', help='To get the values, check out /topics')
+parser_initiatives.add_argument('title', type=str, location='args')
+parser_initiatives.add_argument('status', type=str, location='args', help='To get the values, check out /initiative-status')
+parser_initiatives.add_argument('type', type=str, location='args', help='To get the values, check out /initiative-type')
+parser_initiatives.add_argument('reference', type=str, location='args')
+parser_initiatives.add_argument('place', type=str, location='args')
+parser_initiatives.add_argument('enddate', type=str, location='args', help='Date format must be yyyy-mm-dd')
+parser_initiatives.add_argument('startdate', type=str, location='args', help='Date format must be yyyy-mm-dd')
+parser_initiatives.add_argument('deputy', type=str, location='args', help='To get the values, check out /deputies')
+parser_initiatives.add_argument('author', type=str, location='args', help='To get the values, check out /parliamentary-groups')
+parser_initiatives.add_argument('tags', type=str, action='append', location='args', help='To get the values, check out /topics/id')
+parser_initiatives.add_argument('subtopics', type=str, action='append', location='args', help='To get the values, check out /topics/id')
+parser_initiatives.add_argument('topic', type=str, location='args', help='To get the values, check out /topics')
+parser_initiatives.add_argument('serializer', type=str, location='args', help='To choose the fields of the initiative that will be returned. Options: full(default), simple')
 
+parser_initiative = reqparse.RequestParser()
+parser_initiative.add_argument('serializer', type=str, location='args', help='To choose the fields of the initiative that will be returned. Options: full, simple(default)')
 
 parser_stats = reqparse.RequestParser()
 parser_stats.add_argument('topic', type=str, required=True, location='args', help='To get the values, check out /topics')
@@ -130,6 +134,7 @@ class SearchInitiativeParser:
         self._clean_params()
         self._per_page = self._return_attr_in_params(attrname='per_page', type=int, default=20, clean=True)
         self._page = self._return_attr_in_params(attrname='page', type=int, default=1, clean=True)
+        self._serializer = self._return_attr_in_params(attrname='serializer', type=str, default='', clean=True)
         self._join_tags_and_subtopics_in_params()
         self._join_dates_in_params()
         self._parse_params()
@@ -190,3 +195,26 @@ class SearchInitiativeParser:
     @property
     def params(self):
         return self._params
+
+    @property
+    def serializer(self):
+        if self._serializer == 'full':
+            return InitiativeExtendedSchema
+        return InitiativeSchema
+
+class InitiativeParser():
+    def __init__(self, params):
+        self._params = params
+        self._serializer = self._return_attr_in_params(attrname='serializer', type=str, default='')
+
+    @property
+    def serializer(self):
+        if self._serializer == 'simple':
+            return InitiativeSchema
+        return InitiativeExtendedSchema
+
+    def _return_attr_in_params(self, attrname='', type=str, default=''):
+        if attrname in self._params:
+            attr = self._params[attrname]
+            return type(attr)
+        return default
