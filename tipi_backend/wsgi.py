e7dd3f73d76b602e20a93486372e856655830f70
David Hernández <davidhernandezruiz@gmail.com>
2022-01-27 11:05:45 +0100

    Tries to fix swagger issue.

--- tipi_backend/wsgi.py
+++ tipi_backend/wsgi.py
@@ -1,10 +1,18 @@
 from werkzeug.middleware.proxy_fix import ProxyFix
 
+from flask import render_template
+
 from tipi_backend.app import create_app
 from tipi_backend.settings import Config
 
-
 app = create_app(config=Config)
+
+@app.route('/')
+def swagger_fix():
+    '''Render a SwaggerUI for a given API'''
+    return render_template('swagger-ui.html', title='QHLD API Documentation',
+            specs_url='https://api.quehacenlosdiputados.es/swagger.json')
+
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
 
 
