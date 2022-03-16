e2feb0b6a2eb5886ed75a945fda549d8e71ac869
David Hernández <davidhernandezruiz@gmail.com>
2022-03-16 18:01:48 +0100

    Receives the knowledge base on the form data on the tagging endpoint.

--- tipi_backend/api/parsers.py
+++ tipi_backend/api/parsers.py
@@ -52,7 +52,7 @@ parser_authors.add_argument('name', type=str, location='args', help='Send a name
 parser_tagger = reqparse.RequestParser()
 parser_tagger.add_argument(name='text', type=str, location='form', help='Text to be processed (PREFERENCE)')
 parser_tagger.add_argument(name='file', location='files', help='File to be processed')
-parser_tagger.add_argument('knowledgebase', type=str, location='args', help='To filter the tagger results by knowledge base.')
+parser_tagger.add_argument(name='knowledgebase', type=str, location='form', help='To filter the tagger results by knowledge base.')
 
 
 parser_kb = reqparse.RequestParser()
