e497c29211648e522669746aac0820d842490484
David Hernández <davidhernandezruiz@gmail.com>
2022-04-06 15:36:33 +0200

    Updates method to save scanned documents to use the new scanned model structure.

--- tipi_backend/api/business.py
+++ tipi_backend/api/business.py
@@ -301,11 +301,16 @@ def save_scanned(payload):
         id=generate_id(payload['title'], payload['excerpt'], str(datetime.now())),
         title=payload['title'],
         excerpt=payload['excerpt'],
-        result=ast.literal_eval(payload['result']),
         created=datetime.now(),
         expiration=datetime.fromtimestamp(expiration),
         verified=payload['verified']
     )
+
+    result = payload['result']
+    serialized_result = json.loads(result)
+    tags = serialized_result['tags']
+    for tag in tags:
+        scanned.add_tag(tag['knowledgebase'], tag['topic'], tag['subtopic'], tag['tag'], tag['times'])
 
     saved = scanned.save()
     if not saved:
