6c482541e390e688984e0539446dec4e74b324f1
Alex Ahumada <projectsperminute@gmail.com>
2023-11-07 13:48:56 +0100

    Enables compression with Flask-Compress

--- tipi_backend/app.py
+++ tipi_backend/app.py
@@ -3,6 +3,7 @@ import os
 from os import environ as env
 
 from flask import Flask, Blueprint
+from flask_compress import Compress
 from flask_cors import CORS
 from werkzeug.middleware.proxy_fix import ProxyFix
 
@@ -11,10 +12,14 @@ from tipi_backend.manage_alerts_by_email import alerts_by_email_blueprint
 from tipi_backend.api.endpoints import cache, limiter
 from tipi_backend.api.endpoints.topics import ns as topics_namespace
 from tipi_backend.api.endpoints.deputies import ns as deputies_namespace
-from tipi_backend.api.endpoints.parliamentarygroups import ns as parliamentarygroups_namespace
+from tipi_backend.api.endpoints.parliamentarygroups import (
+    ns as parliamentarygroups_namespace,
+)
 from tipi_backend.api.endpoints.initiatives import ns as initiatives_namespace
 from tipi_backend.api.endpoints.initiative_types import ns as initiativetypes_namespace
-from tipi_backend.api.endpoints.initiative_status import ns as initiativestatus_namespace
+from tipi_backend.api.endpoints.initiative_status import (
+    ns as initiativestatus_namespace,
+)
 from tipi_backend.api.endpoints.places import ns as places_namespace
 from tipi_backend.api.endpoints.stats import ns as stats_namespace
 from tipi_backend.api.endpoints.footprint import ns as footprint_namespace
@@ -27,36 +32,44 @@ from tipi_backend.api.restplus import api
 
 def create_app(config=Config):
     app = Flask(__name__)
+    Compress(app)
     app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1)
     app.config.from_object(config)
     initialize_app(app)
 
-    logging_conf_path = os.path.normpath(os.path.join(os.path.dirname(__file__), '../logging.conf'))
+    logging_conf_path = os.path.normpath(
+        os.path.join(os.path.dirname(__file__), "../logging.conf")
+    )
     logging.config.fileConfig(logging_conf_path)
     log = logging.getLogger(__name__)
-    log.info('>>>>> Starting development server at http://{}/ <<<<<'.format(app.config['SERVER_NAME']))
+    log.info(
+        ">>>>> Starting development server at http://{}/ <<<<<".format(
+            app.config["SERVER_NAME"]
+        )
+    )
     return app
 
 
 def add_namespaces(app):
-    namespaces = [topics_namespace,
-                  deputies_namespace,
-                  parliamentarygroups_namespace,
-                  initiatives_namespace,
-                  initiativetypes_namespace,
-                  initiativestatus_namespace,
-                  places_namespace,
-                  stats_namespace,
-                  footprint_namespace,
-                  voting_namespace,
-                  tagger_namespace,
-                  scanned_namespace,
+    namespaces = [
+        topics_namespace,
+        deputies_namespace,
+        parliamentarygroups_namespace,
+        initiatives_namespace,
+        initiativetypes_namespace,
+        initiativestatus_namespace,
+        places_namespace,
+        stats_namespace,
+        footprint_namespace,
+        voting_namespace,
+        tagger_namespace,
+        scanned_namespace,
     ]
     if Config.USE_ALERTS:
         namespaces.append(alerts_namespace)
 
     for ns in namespaces:
-        if ns.name in env.get('EXCLUDE_NAMESPACES', []):
+        if ns.name in env.get("EXCLUDE_NAMESPACES", []):
             continue
         api.add_namespace(ns)
 
@@ -64,7 +77,7 @@ def add_namespaces(app):
 def initialize_app(app):
     cache.init_app(app, config=Config.CACHE)
     limiter.init_app(app)
-    blueprint = Blueprint('api', __name__)
+    blueprint = Blueprint("api", __name__)
     CORS(blueprint)
     api.init_app(blueprint)
     add_namespaces(app)
@@ -76,9 +89,7 @@ def initialize_app(app):
 def main():
     app = create_app(Config)
     app.run(
-        host=app.config['IP'],
-        port=app.config['PORT'],
-        debug=app.config['FLASK_DEBUG']
+        host=app.config["IP"], port=app.config["PORT"], debug=app.config["FLASK_DEBUG"]
     )
 
 
